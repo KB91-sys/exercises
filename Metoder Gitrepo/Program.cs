4e7503119be1836ae8fb29dc7d5e0da7d274d263
Kristofer <kristofer_b_1991@hotmail.com>
2019-09-22 16:14:37 +0200

    Metoder

---
+++ Metoder Gitrepo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Metoder_Gitrepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("***METODER***\n\n");
+            Console.Write("Övning 1:\n");
+
+            
+
+
+
+
+
+        }
+    }
+}
